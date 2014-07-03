6ffef9e72f6b377f88edaca76037b50e9070c0d9
Robin McCorkell <rmccorkell@karoshi.org.uk>
2014-07-03 20:48:11 +0100

    Fix autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Create ChangeLog from git
-if [[ -d .git ]] && which git > /dev/null; then
+if which git > /dev/null && git rev-parse; then
 	[[ -e ChangeLog ]] && rm -f ChangeLog
 	git log --pretty=medium --abbrev-commit > ChangeLog
 else
-	touch Changelog #suppress errors with a missing ChangeLog
+	touch ChangeLog #suppress errors with a missing ChangeLog
 fi
 
 # Prepare the source files for packaging
